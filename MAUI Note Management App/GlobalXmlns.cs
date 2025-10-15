d3f16378b71995541625604e1a102b169a73da39
Elinor Tchamo <tchamoelii@gmail.com>
2025-10-15 11:27:00 +0200

    Add project files.

---
+++ MAUI Note Management App/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MAUI_Note_Management_App")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MAUI_Note_Management_App.Pages")]
