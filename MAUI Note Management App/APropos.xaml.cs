933e00a6f01dcbe6f8931acd10fb601aa63964ed
Elinor Tchamo <tchamoelii@gmail.com>
2025-10-15 11:29:49 +0200

    Feat: About page done

---
+++ MAUI Note Management App/APropos.xaml.cs
@@ -0,0 +1,14 @@
+namespace MAUI_Note_Management_App;
+
+public partial class APropos : ContentPage
+{
+	public APropos()
+	{
+		InitializeComponent();
+	}
+
+    private async void APropos_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.Default.OpenAsync("https://www.3il-ingenieurs.fr/");
+    }
+}
